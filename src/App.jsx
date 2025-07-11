b1c80d965d5ce3c6ac1016620ee110022fa0b794
Sandesh Dadibude <sandeshdadibude28042004@.com>
2025-07-11 08:50:02 +0530

    Implement Dynamic Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,20 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
-import Hello from './Hello'
-import Greet from './Greet';
-import Counter from './Counter'
-import ButtonClick from './ButtonClick';
-import Welcome from './Welcome';
-import UserList from './UserList';
-import TodoList from './TodoList';
-import SimpleForm from './SimpleForm';
-import LoginForm from './LoginForm';
-import EmailForm from './EmailForm';
-import PostList from './PostList';
+import User from './User'
 
 function App() {
 
   return (
-    <div>
-      <h1>Hello React</h1>
-      <p>Welcome to my first app!</p>
-      <Hello />
-      <Greet name="sandesh" />
-      <Greet name="React lerner" />
-      <Counter />
-      <ButtonClick />
-      <Welcome isLoggedIn={true} />
-      <Welcome isLoggedIn={false} />
-      <UserList />
-      <TodoList />
-      <SimpleForm />
-      <LoginForm />
-      <EmailForm/>
-      <PostList/>
-    </div>
-  );
+    <Router>
+
+      <Routes>
+        <Route path='/user/:id' element={<User/>} />
+        <Route path='*' element={<h2>404 Page not found</h2>} />
+      </Routes>
+
+    </Router>
+  )
+
 }
 
 export default App
